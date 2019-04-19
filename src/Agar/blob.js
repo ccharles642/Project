bfa4e4fcfd527f27309ae11088b839d0290b023f
josuequi <Jo$ue0810>
2019-04-19 17:32:50 -0400

    Updated html for demo 2

--- src/Agar/blob.js
+++ src/Agar/blob.js
@@ -2,30 +2,21 @@
 function Blob(x, y, r) {
     this.pos = createVector(x, y);
     this.r = r;
-    // giving a velocity and it new velocity
     this.vel = createVector(0,0);
 
     this.update = function() {
         var newvel = createVector(mouseX-width/2, mouseY-height/2);
         newvel.setMag(3);
-        // the turning with the decimal below is determing the gradualness of the turning the higher the more fast and rapid.
         this.vel.lerp(newvel, 0.3);
         this.pos.add(this.vel);
     }
 
-// writing a function named eat which receives another blob,
     this.eats = function(other) {
-        // distance between this blobs vector and other blobs position
         var d = p5.Vector.dist(this.pos, other.pos);
-        // if this distance is less than this blobs radius plus the other blobs radius
 
-        //having radius from two different objects touch if they are overlapping then less than
         if (d < this.r + other.r) {
-            // sum of the two areas
             var sum = PI * this.r * this.r + PI * other.r * other.r;
-            // solve for r
             this.r = sqrt(sum / PI);
-            //this.r += other.r;
             return true;
         } else {
             return false;
--- src/Agar/sketch.js
+++ src/Agar/sketch.js
@@ -7,10 +7,8 @@ var zoom = 1;
 
 function setup() {
     createCanvas(700, 700);
-    // start at 0 , 0
     blob = new Blob(0, 0, 20);
     for (var i = 0; i < 500; i++) {
-        // x and y below will give me a bigger canvas to work with
         var x = random(-width,width);
         var y = random(-height,height);
         blobs[i] = new Blob(x, y, 16);
@@ -19,22 +17,17 @@ function setup() {
 
 function draw() {
     background(210);
-// the world is drawn relative to the blob itself
-// translate to the center
+
     translate(width/2, height/2);
     var newzoom = 64 / blob.r;
 
-    // blobs dont actually row in size they just zoom out the frame everytime a new one is eaten so it looks like it is getting bigger.
-// lerp is linear enterplation moving slowly to a new value so does not jump background,
-// want to be just a little bit 10% of the new value
+
     zoom = lerp(zoom, newzoom, 0.1);
     scale(zoom);
     translate(-blob.pos.x, -blob.pos.y);
 
-// looping through the array backwards, to remove thing
     for (var i = blobs.length-1; i >=0; i--) {
         blobs[i].show();
-        // removing the blob if it gets eaten
         if (blob.eats(blobs[i])) {
             blobs.splice(i, 1);
         }
